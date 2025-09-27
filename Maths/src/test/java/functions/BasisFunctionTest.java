bb7c46743a69599a6600000ba934bbc12a825b6e
islamgulov <islamgulov_uchebniy@mail.ru>
2025-09-27 18:01:29 +0400

    ЛР2, Задание1, П4 (X или Y, 2/2), Студент Y(Исламгулов Кирилл)

--- Maths/src/test/java/functions/BasisFunctionTest.java
+++ Maths/src/test/java/functions/BasisFunctionTest.java
@@ -8,13 +8,13 @@ class BasisFunctionTest {
 
     void testDegree1() {
         double[] knots = {0, 1, 2, 3};
-        // Проверяем N_{0,1}
+        //Проверяем N_{0,1}
         assertEquals(1, BasisFunction.N(0, 1, 0.5, knots), 0.0001);
         assertEquals(0, BasisFunction.N(0, 1, 1.5, knots), 0.0001);
     }
     
     void testPartitionOfUnity() {
-        double[] knots = {0, 0, 0, 1, 2, 3, 3, 3}; // кубический сплайн
+        double[] knots = {0, 0, 0, 1, 2, 3, 3, 3}; // Кубический сплайн
         int degree = 3;
         double t = 1.5;
 
@@ -23,7 +23,7 @@ class BasisFunctionTest {
             sum += BasisFunction.N(i, degree + 1, t, knots);
         }
 
-        // сумма базисных функций должна быть 1
+        //Сумма базисных функций должна быть 1
         assertEquals(1, sum, 0.0001);
     }
 }
