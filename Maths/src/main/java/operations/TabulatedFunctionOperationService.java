21b1678397d6083e05c821795b54f523c4c2101f
islamgulov <islamgulov_uchebniy@mail.ru>
2025-10-10 14:24:51 +0400

    ЛР3, Задание2, П4 (Y), студент Y(Исламгулов Кирилл)

--- Maths/src/main/java/operations/TabulatedFunctionOperationService.java
+++ Maths/src/main/java/operations/TabulatedFunctionOperationService.java
@@ -1,8 +1,20 @@
 package operations;
 
 import functions.*;
+import functions.factory.TabulatedFunctionFactory;
+import exceptions.InconsistentFunctionsException;
 
 public class TabulatedFunctionOperationService {
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService() {
+        this.factory = new functions.factory.ArrayTabulatedFunctionFactory();
+    }
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+    public TabulatedFunctionFactory getFactory() { return factory; }
+    public void setFactory(TabulatedFunctionFactory factory) { this.factory = factory; }
 
     public Point[] asPoints(TabulatedFunction tabulatedFunction) {
         Point[] pts = new Point[tabulatedFunction.getCount()];
@@ -11,5 +23,31 @@ public class TabulatedFunctionOperationService {
             pts[i++] = p;
         }
         return pts;
+    }
+
+    private interface BiOperation { double apply(double u, double v); }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation op) {
+        if (a.getCount() != b.getCount()) throw new InconsistentFunctionsException("Different counts");
+        Point[] pa = asPoints(a);
+        Point[] pb = asPoints(b);
+        int n = pa.length;
+        double[] xValues = new double[n];
+        double[] yValues = new double[n];
+        for (int i = 0; i < n; i++) {
+            if (Double.compare(pa[i].x, pb[i].x) != 0) {
+                throw new InconsistentFunctionsException("x values differ at index " + i);
+            }
+            xValues[i] = pa[i].x;
+            yValues[i] = op.apply(pa[i].y, pb[i].y);
+        }
+        return factory.create(xValues, yValues);
+    }
+
+    public TabulatedFunction add(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u + v);
+    }
+    public TabulatedFunction subtract(TabulatedFunction a, TabulatedFunction b) {
+        return doOperation(a, b, (u, v) -> u - v);
     }
 }
